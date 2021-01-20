d205562a31b97fe085c29d6ae03d76cc58f87905
caquijano <carlosquijano98@gmail.com>
2021-01-20 14:46:41 -0500

    Mejoras ventas

--- src/components/Sales/SaleDetail.ts
+++ src/components/Sales/SaleDetail.ts
@@ -1,7 +1,7 @@
 export interface SaleDetail{
     _id?: string;
-    invoice?: string;
-    cliente?: string;
+    invoice?: number;
+    client: string;
     date: string | Date;
     createAt?: string | Date;
     updateAt?: string | Date;
--- src/components/Sales/SaleForm.tsx
+++ src/components/Sales/SaleForm.tsx
@@ -5,6 +5,8 @@ import { Sale } from "./Sale";
 import * as saleService from "./saleService";
 import { SaleDetail } from "./SaleDetail";
 import * as saleDetailService from "./saleDetailService";
+import { toast } from "react-toastify";
+import { useHistory, } from "react-router-dom";
 
 import { BsX } from "react-icons/bs";
 
@@ -14,18 +16,26 @@ interface IArt {
   amount: number;
   priceSale: number;
 }
+/*interface IPer {
+  client: string;
+  date: string | Date;
+  invoice: number;
+}*/
+
 const SaleForm = () => {
+  const history = useHistory();
   const [items, setItems] = useState<Item[]>([]);
   const [item, setItem] = useState<Item>();
   const [saleD, setSaleD] = useState<SaleDetail[]>([]);
+  const [invoice, setInvoice] = useState(0)
   //const [sDetail, setSDetail] = useState<SaleDetail>();
-  const [load, setLoad] = useState(true);
-  const [article, setArticle] = useState<IArt[]>([]);
-  const [newArticle, setNewArticle] = useState<IArt>({
+  const [article, setArticle] = useState<Sale[]>([]);
+  const [newArticle, setNewArticle] = useState<Sale>({
     productId: "",
     amount: 0,
     priceSale: 0,
   });
+  
   const handleSubmit2 = (e: FormElemEvent): void => {
     e.preventDefault();
     addArticle(
@@ -38,8 +48,8 @@ const SaleForm = () => {
     });
   };
 
-  const addArticle = (art: IArt): void => {
-    const newArticles: IArt[] = [
+  const addArticle = (art: Sale): void => {
+    const newArticles: Sale[] = [
       ...article,
       {
         productId: art.productId,
@@ -50,7 +60,7 @@ const SaleForm = () => {
     setArticle(newArticles);
   };
   const removeArticle = (i: number): void => {
-    const newArt: IArt[] = [...article];
+    const newArt: Sale[] = [...article];
     newArt.splice(i, 1);
     setArticle(newArt);
   };
@@ -63,7 +73,21 @@ const SaleForm = () => {
   const loadSalesD = async () => {
     const res = await saleDetailService.getSaleDetails();
     setSaleD(res.data);
+    changeDetail();
   };
+  const changeDetail = async () => {
+    
+    if(saleD.length === 0){
+      setInvoice(1)
+    }else{
+      setInvoice(saleD.length)
+    }
+  }
+  const [person, setPerson] = useState<SaleDetail>({
+    client: "",
+    date: "",
+    invoice: invoice,
+  })
   const changeStock = async (e: ChangeEvent<HTMLSelectElement>) => {
     const res = await itemService.getItem(e.target.value);
     setItem(res.data);
@@ -74,15 +98,29 @@ const SaleForm = () => {
     setNewArticle({ ...newArticle, [e.target.name]: e.target.value });
     console.log(newArticle);
   };
+  const handleAdd2 = async (
+    e: ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    setPerson({ ...person, [e.target.name]: e.target.value });
+    console.log(newArticle);
+  };
 
   const onChange2 = async (e: ChangeEvent<HTMLSelectElement>) => {
     handleAdd(e);
     changeStock(e);
   };
+
+  const save = async () => {
+    console.log("hellowprl")
+    await saleDetailService.createSaleDetail(person);
+    await saleService.createSale(article)
+    toast.success('Articulo agregado satisfactoriamente')
+    history.push('/items')
+  }
   useEffect(() => {
     loadItems();
     loadSalesD();
-  }, [load]);
+  }, []);
 
   return (
     <div>
@@ -98,42 +136,41 @@ const SaleForm = () => {
         <div className="form-group col-lg-5"></div>
         <br />
         <div className="card border-primary mb-3" style={{ width: 1000 }}>
-          <div className="card-header">Sistema de ventas</div>
+          <div className="card-header"><h4>Nueva venta</h4></div>
           <br />
-          <h4>Nueva venta</h4>
-          <br />
+          
           <div className="form-group col-lg-12 row">
             <label className="col-form-label col-lg-1" htmlFor="inputDefault">
               Cliente{" "}
             </label>
             <input
+            name="client"
               type="text"
               className="form-control col-lg-4"
               placeholder="Nombre del cliente"
+              onChange={handleAdd2}
             />
             <label className="col-form-label col-lg-1" htmlFor="inputDefault">
               Fecha:{" "}
             </label>
             <input
+            name="date"
               type="Date"
               className="form-control col-lg-2"
               placeholder="Nombre del cliente"
+              onChange={handleAdd2}
             />
             <label className="col-form-label col-lg-2" htmlFor="inputDefault">
               Numero de factura:
             </label>
-            <input
-              type="text"
-              className="form-control col-lg-1"
-              placeholder="001"
-            />
+            <input className="form-control col-lg-1" onChange={handleAdd2} style={{color: "blueviolet"}} value={invoice} placeholder={ `${invoice}`}/>
           </div>
           <div className="card-body">
             <div
               className="card border-success col-lg-12"
               style={{ maxWidth: "60rem" }}
             >
-              <form id="miForm" onSubmit={handleSubmit2}>
+              <form id="articleForm" onSubmit={handleSubmit2}>
                 <div className="card-body">
                   <div className="form-group row col-lg-12">
                     <div className="form-group col-lg-4">
@@ -212,7 +249,7 @@ const SaleForm = () => {
                       />
                     </div>
                   </div>
-                  <button type="submit" className="btn btn-primary">
+                  <button form="articleForm" type="submit" className="btn btn-primary">
                     Agregar
                   </button>
                   <br />
@@ -278,14 +315,15 @@ const SaleForm = () => {
             </div>
 
             <button
-              type="submit"
+              type="button"
+              onClick={()=> save}
               className="btn btn-success"
               style={{ margin: 20 }}
             >
               Guardar
             </button>
-            <button type="submit" className="btn btn-danger" style={{}}>
-              Guardar
+            <button type="submit"   className="btn btn-danger" style={{}}>
+              Cancelar
             </button>
           </div>
         </div>
